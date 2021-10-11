dc832ea3f8b71115d06142e401974e6280fa6b15
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2021-10-11 13:04:21 -0700

    Adds an exception for a "No bounce" role

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,36 @@ use tokio::time::sleep;
 
 struct Handler;
 
+async fn handle_voice(ctx: Context, opt_gid: Option<GuildId>, st: VoiceState) -> Option<()> {
+    let gid = opt_gid?;
+    let member = st.member?;
+    let channel = st.channel_id?;
+    let name = channel.name(&ctx).await?;
+    if let Some(roles) = member.roles(&ctx).await {
+        if let Some(_r) = roles.iter().filter(|r| r.name == "No bounce").next() {
+            return Some(());
+        }
+    }
+    if "OnlyCams (required)".to_string() == name && !st.self_video {
+        // TODO spawn a thread to check the status
+        // again in 3 seconds
+        sleep(Duration::from_secs(10)).await;
+        if let Some(guild) = gid.to_guild_cached(&ctx).await {
+            // lookup member.user.id in
+            // guild.voice_states
+            // then check if the video is still disabled
+            // if so then...
+            if let Some(new_st) = guild.voice_states.get(&member.user.id) {
+                if !new_st.self_video {
+                    member.disconnect_from_voice(&ctx).await;
+                }
+            }
+        }
+    }
+
+    Some(())
+}
+
 // TODO: I think I just need to set permissions for the bot
 // Also add some logging
 #[async_trait]
@@ -21,29 +51,10 @@ impl EventHandler for Handler {
         &self,
         ctx: Context,
         opt_gid: Option<GuildId>,
-        oldVcSt: Option<VoiceState>,
+        old_vc_St: Option<VoiceState>,
         st: VoiceState,
     ) {
-        if let (Some(gid), Some(member), Some(channel)) = (opt_gid, st.member, st.channel_id) {
-            if let Some(name) = channel.name(&ctx).await {
-                if "OnlyCams (required)".to_string() == name && !st.self_video {
-                    // TODO spawn a thread to check the status
-                    // again in 3 seconds
-                    sleep(Duration::from_secs(10)).await;
-                    if let Some(guild) = gid.to_guild_cached(&ctx).await {
-                        // lookup member.user.id in
-                        // guild.voice_states
-                        // then check if the video is still disabled
-                        // if so then...
-                        if let Some(new_st) = guild.voice_states.get(&member.user.id) {
-                            if !new_st.self_video {
-                                member.disconnect_from_voice(&ctx).await;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        handle_voice(ctx, opt_gid, st).await;
     }
 }
 
